e0338adc9557caf0b6e85f765ce54da7ea5e71fe
Max Hess <max.hess@fmi.ch>
2026-07-23 11:37:39 +0200

    run: new params

--- convert_datasets.sh
+++ convert_datasets.sh
@@ -2,9 +2,9 @@
 set -euo pipefail
 
 INPUT_BASE="/links/groups/liberali/ldp_dev/data_ls1/data_ls1"
-OUTPUT_BASE="/links/groups/liberali/ldp_dev/data_zarr"
+OUTPUT_BASE="/links/groups/liberali/ldp_dev/data_zarr/Max2"
 
-for dataset in 000 001 002; do
+for dataset in 002; do
     echo "=== Converting $dataset ==="
     convert-ls1-datasets "$INPUT_BASE/$dataset" "$OUTPUT_BASE/$dataset" --overwrite
 done
