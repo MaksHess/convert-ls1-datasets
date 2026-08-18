b596f99058c50cd87cb216d1d0f282c0e57ca4cb
Max Hess <hess.max.timo@gmail.com>
2026-08-18 10:16:44 +0200

    fix: typo

--- export_zenodo_dataset.sh
+++ export_zenodo_dataset.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 INPUT_BASE="/links/groups/liberali/ldp_dev/data_zarr/Max/002"
 OUTPUT_BASE="/links/groups/liberali/ldp_dev/data_zenodo"
 
-for dataset in tiny "tiny-lowT" "tini-varT" "mini" "mini-lowT" "mini-varT"; do
+for dataset in tiny "tiny-lowT" "tiny-varT" "mini" "mini-lowT" "mini-varT" "full"; do
     echo "=== Exporting $dataset ==="
     export-zenodo-dataset "$INPUT_BASE" "$OUTPUT_BASE" -n "$dataset" --overwrite
 done
